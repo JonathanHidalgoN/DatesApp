4a6cf7db5310328dfb6d4236d7b2924d24469ab5
Jonathan <joni7840850@gmail.com>
2024-09-02 11:03:49 -0600

    Add implementation to create web token

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,38 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API;
+
+public class TokenService(IConfiguration config) : ITokenService{
+
+    /**
+        * This method is used to create a token
+        * @param user: The user object
+        * @return The token
+    */
+    public string createToken(AppUser user){
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Token key not found");
+        if(tokenKey.Length < 64){
+            throw new Exception("Token key is too short");
+        }
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>{
+            new Claim(ClaimTypes.NameIdentifier, user.userName)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor{
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
