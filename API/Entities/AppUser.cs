17c3068dfa045b20adca0ffecc68beda726cdb53
Jonathan <joni7840850@gmail.com>
2024-09-02 09:47:51 -0600

    Add passwordhash attributes

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,10 @@
-
 namespace API;
 
 public class AppUser{
     public int id{get; set;}
     public required string userName {get; set;}
+
+    public required byte[] passwordHash {get; set;}
+
+    public required byte[] passwordSalt {get; set;}
 }
