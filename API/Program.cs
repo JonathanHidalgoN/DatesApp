6a1db2ded568716fd8e6695ae5b8fb1af4f7babb
Jonathan <joni7840850@gmail.com>
2024-09-03 22:14:54 -0600

    Add middleware to workflow

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationService(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
-
+app.UseMiddleware<ApiMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
 WithOrigins("http://localhost:4200", "https://localhost:4200/"));
 
