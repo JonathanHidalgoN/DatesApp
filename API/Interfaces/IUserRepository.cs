3a4e660ad91f560163e55b5b0b2577ac00c6ccd9
Jonathan <joni7840850@gmail.com>
2024-09-05 12:55:00 -0600

    Add method signature

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,48 @@
+namespace API;
+
+public interface IUserRepository{
+
+    /**
+    * Get all users
+    * @return A list of all users
+    */
+    void Update(AppUser user);
+
+    /**
+    * Update a user
+    * @param user The user to update
+    */
+    Task<bool> SaveAllAsync();
+
+    /**
+    * Save all changes to the database
+    * @return True if changes were saved, false otherwise
+    */
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+
+    /**
+    * Get all users
+    * @return A list of all users
+    */
+    Task<AppUser?> GetUserByIdAsync(int id);
+
+    /**
+    * Get a user by their ID
+    * @param id The ID of the user
+    * @return The user with the given ID
+    */
+    Task<AppUser?> GetUserByUserNameAsync(string userName);
+
+    /**
+    * Get a user by their username
+    * @param userName The username of the user
+    * @return The user with the given username
+    */
+    Task<IEnumerable<MembersDto>> GetMembersAsync();
+
+    /**
+    * Get all members
+    * @return A list of all members
+    */
+    Task<MembersDto?> GetMemberAsync(string username);
+}
