5115c8f0ec06186440040e43c8e1ecd3158b15ee
Jonathan <joni7840850@gmail.com>
2024-09-05 12:56:25 -0600

    Add attibutes to user Dto

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,7 +4,30 @@ public class AppUser{
     public int id{get; set;}
     public required string userName {get; set;}
 
-    public required byte[] passwordHash {get; set;}
+    public byte[] passwordHash {get; set;} = [];
 
-    public required byte[] passwordSalt {get; set;}
+    public byte[] passwordSalt {get; set;} = [];
+
+    public DateOnly dateOfBirth {get; set;}
+
+    public required string knownAs {get; set;}
+
+    public DateTime created {get; set;} = DateTime.UtcNow;
+
+    public DateTime lastActive {get; set;} = DateTime.UtcNow;
+
+    public required string gender {get; set;}
+
+    public string? introduction {get; set;}
+
+     public string? interest {get; set;}
+
+     public string? lookingFor {get; set;}
+
+    public required string city {get; set;}
+
+    public required string country {get; set;}
+
+    public List<Photo> Photos {get; set;} = [];
+
 }
