a2830be64e0ce4a2fc4751eb2698b85986d622db
Jonathan <joni7840850@gmail.com>
2024-09-06 09:52:57 -0600

    Add migrations

---
+++ API/Data/Migratios/20240904145949_UpdatedUserEntity.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migratios
+{
+    /// <inheritdoc />
+    public partial class UpdatedUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "city",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "dateOfBirth",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "gender",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "interest",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "introduction",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "knownAs",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "lastActive",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "lookingFor",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    url = table.Column<string>(type: "TEXT", nullable: false),
+                    isMain = table.Column<bool>(type: "INTEGER", nullable: false),
+                    publicId = table.Column<string>(type: "TEXT", nullable: true),
+                    AppUserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photos",
+                column: "AppUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "city",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "created",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "dateOfBirth",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "interest",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "introduction",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "knownAs",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "lastActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "lookingFor",
+                table: "Users");
+        }
+    }
+}
