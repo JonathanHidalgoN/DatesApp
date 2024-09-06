91214196fab104ab155610298d223f6160e86b60
Jonathan <joni7840850@gmail.com>
2024-09-06 12:54:40 -0600

    Schema migrations

---
+++ API/Data/Migratios/20240906162811_changeusernamecasing.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migratios
+{
+    /// <inheritdoc />
+    public partial class changeusernamecasing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "userName",
+                table: "Users",
+                newName: "username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "Users",
+                newName: "userName");
+        }
+    }
+}
---
+++ API/Data/Migratios/20240906174509_ChangeInterestsName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migratios
+{
+    /// <inheritdoc />
+    public partial class ChangeInterestsName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "interest",
+                table: "Users",
+                newName: "interests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "interests",
+                table: "Users",
+                newName: "interest");
+        }
+    }
+}
