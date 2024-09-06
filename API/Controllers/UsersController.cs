b36b77abb8e5c77c53f00a520083ccc8f6536bce
Jonathan <joni7840850@gmail.com>
2024-09-06 12:54:54 -0600

    Add endpoint to update users

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,12 @@
- using AutoMapper;
+using System.Security.Claims;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController{       
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController{       
     /*
     Get all the users from the database
     */
@@ -26,6 +27,21 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
             return NotFound();
         }
         return user;
+    }
+
+    /*
+    Update the user
+    param memberUpdateDto : The data to update
+    */
+    [HttpPut]
+    public async Task<ActionResult> updateUser(MemberUpdateDto memberUpdateDto){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if(username == null) return BadRequest("User not found");
+        var user = await userRepository.GetUserByUserNameAsync(username);
+        if(user == null) return BadRequest("User not found");
+        mapper.Map(memberUpdateDto, user);
+        if(await userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
     }
 
 }
