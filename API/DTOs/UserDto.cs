52d19ec23367741661da8463429e688fec523f14
Jonathan <joni7840850@gmail.com>
2024-09-09 12:03:08 -0600

    Add photourl attribute

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -3,4 +3,6 @@ namespace API;
 public class UserDto{
     public required string username { get; set; }
     public required string token { get; set; }
+
+    public string? photoUrl { get; set; }
 }
