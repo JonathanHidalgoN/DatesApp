b15fb0a9249ab6044a2ec114ffee2b9d24af2a81
Jonathan <joni7840850@gmail.com>
2024-09-12 11:33:21 -0600

    Add new attributes to dtos

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,4 +9,19 @@ public class RegisterDto{
     [Required]
     [MaxLength(40)]
     public string password { get; set; } = string.Empty;
+
+    [Required]
+    public string? knowAs { get; set; }
+
+    [Required]
+    public string? gender { get; set; }
+
+    [Required]
+    public string? dateOfBirth { get; set; }
+
+    [Required]
+    public string? city { get; set; }
+
+    [Required]
+    public string? contry { get; set; }
 }
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -5,4 +5,6 @@ public class UserDto{
     public required string token { get; set; }
 
     public string? photoUrl { get; set; }
+
+    public required string? knowAs { get; set; }
 }
