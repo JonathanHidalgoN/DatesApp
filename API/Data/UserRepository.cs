a89eb93a8af9149c0745ffa4e08fa111c9e2adc0
Jonathan <joni7840850@gmail.com>
2024-09-17 08:23:51 -0600

    Add logic to query with filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -7,16 +7,34 @@ namespace API;
 
 public class UserRepository(DataContext context, IMapper mapper) : IUserRepository
 {
+    
     public async Task<MembersDto?> GetMemberAsync(string username){
         return await context.Users.Where(x => x.username == username)
         .ProjectTo<MembersDto>(mapper.ConfigurationProvider)
         .SingleOrDefaultAsync();
     }
 
-    public async Task<IEnumerable<MembersDto>> GetMembersAsync(){
-        return await context.Users
-        .ProjectTo<MembersDto>(mapper.ConfigurationProvider)
-        .ToListAsync();
+    public async Task<PagedList<MembersDto>> GetMembersAsync(UserParams userParam){
+        var query = context.Users.AsQueryable();
+        query = query.Where(x => x.username != userParam.currentUsername);
+        if(userParam.gender != null){
+            query = query.Where(x => x.gender == userParam.gender);
+        }
+
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParam.MaxAge-1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParam.MinAge));
+
+        query = query.Where(x => x.dateOfBirth >= minDob && x.dateOfBirth <= maxDob);
+
+        query = userParam.orderBy switch
+        {
+            "created" => query.OrderByDescending(x => x.created),
+            _ => query.OrderByDescending(x => x.lastActive)
+            
+        };
+
+        return await PagedList<MembersDto>.createAsync(query.ProjectTo<MembersDto>(mapper.ConfigurationProvider)
+        , userParam.PageNumber, userParam.PageSize);
     }
 
     public async Task<AppUser?> GetUserByIdAsync(int id)
