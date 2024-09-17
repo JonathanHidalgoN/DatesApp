93ebc8faba6e6644d9725c34abb8a0c9db727b59
Jonathan <joni7840850@gmail.com>
2024-09-17 08:22:37 -0600

    Add component to check user activity

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")]
 public class BaseApiController: ControllerBase{
