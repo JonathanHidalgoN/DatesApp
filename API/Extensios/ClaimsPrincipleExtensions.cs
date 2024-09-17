b569b6d98ab6bbe25e71392be7531f0bb6164147
Jonathan <joni7840850@gmail.com>
2024-09-17 08:24:29 -0600

    Add method to get user id

--- API/Extensios/ClaimsPrincipleExtensions.cs
+++ API/Extensios/ClaimsPrincipleExtensions.cs
@@ -5,8 +5,14 @@ namespace API;
 public static class ClaimsPrincipleExtensions{
 
     public static string getUsername(this ClaimsPrincipal user){
-        var username = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        var username = user.FindFirstValue(ClaimTypes.Name);
         if(username == null) throw new Exception("User not found");
         return username;
+    }
+
+       public static int getUserId(this ClaimsPrincipal user){
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
+        ?? throw new Exception("User not found"));
+        return userId;
     }
 }
